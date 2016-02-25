24fefda4282c99f8efee714f0fd262a4e4045426
PWMCats <pereza@oregonstate.edu>
2016-02-25 15:52:23 -0800

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,11 @@
+import smtplib
+fromaddr = 'pwmcats@gmail.com'
+toaddrs  = 'pwmcats@gmail.com'
+msg = 'This is a lightning warning!'
+username = 'pwmcats@gmail.com'
+password = 'pussymoneyweed'
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
